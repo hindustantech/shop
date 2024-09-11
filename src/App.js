21cfc77ccb169c135865bb3b211fb22735767014
hindustantech <145006668+hindustantech@users.noreply.github.com>
2024-09-11 00:37:30 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Home } from './Componenet/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Shop } from './Componenet/Shop';
+import SignUp from './Componenet/SignUp';
+import Login from './Componenet/Login';
+import Dashboard from './Componenet/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop/>} /> 
+          <Route path="/dahboard" element={<Dashboard/>} /> 
+          <Route path="/profile" element={<SignUp/>} /> 
+          <Route path="/Login" element={<Login/>} /> 
+      
+          
+        </Routes>
+      </Router>
+      
+
+    </>
+
+
   );
 }
 
---
+++ src/Componenet/Login.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+const Login = () => {
+  return (
+    <>
+
+      <div className='green-box App'>
+
+        <div className="login-container mx-auto">
+
+          <img src="/asset/logo/3.png" className="img-fluid close-png" alt="" />
+          <div className="row mt-4">
+            <div className="col-12 ">
+              <img src="/asset/logo/2.png" alt="Logo" className="logo me-2 logo-h" />
+              <h1 className="login-text ">Login</h1>
+            </div>
+          </div>
+
+
+          <div className="row justify-content-center mt-4">
+            <div className="col-12 col-md-6 text-center">
+              <img src="/asset/logo/4.png" alt="" className="img-fluid logo-h" style={{height: '250px',}} />
+            </div>
+          </div>
+
+
+          <div className="row justify-content-center mt-4">
+            <div className="col-12 col-md-4">
+              <form className='loginForm'>
+                <div className="mb-3 input-fi">
+                  <img src="/asset/logo/1.png" className='logo-e' alt="" />
+                  <input type="email" className="email input-login" id="email" placeholder="Email" />
+                </div>
+                <div className="mb-3 input-fi">
+                  <img src="/asset/logo/5.png" className='logo-e' alt="" />
+                  <input type="password" className="password input-login" id="password" placeholder=" password" />
+                </div>
+                <button type="submit" className="btn-color btn">Login <i className="fas fa-arrow-right mx-3"></i> </button>
+              </form>
+            </div>
+            <div className='mt-5 '>
+              <a className='text-black' href="/">Forgot Password?</a>
+              <p className=''>Don’t have account?
+                <span className=''><Link to="/profile" className='text-black'> sign up</Link></span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
+};
+
+export default Login;
