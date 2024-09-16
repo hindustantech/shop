a8389ed1ef3034ec2e28517561493167ca8cd31f
hindustantech <145006668+hindustantech@users.noreply.github.com>
2024-09-15 23:20:12 -0700

    Fix css

--- src/Componenet/Dashboard.js
+++ src/Componenet/Dashboard.js
@@ -10,7 +10,7 @@ const Dashboard = () => {
                 <img src="/asset/design/25.png" className='l-h mx-3' alt="Company Logo 2" />
             </div>
             <div className="dashboard-body">
-                <div class="row px-4 mt-3">
+                <div class="row px-2 mt-3">
                     <h2>Wallets</h2>
                     <div class="col-6 ">
                         <div className='cardBox '>
@@ -42,7 +42,7 @@ const Dashboard = () => {
                     </div>
                 </div>
 
-                <div class="row px-4 mt-3">
+                <div class="row px-2 mt-3">
                     <h2>Income</h2>
                     <div class="col-12 mt-3">
                         <div className='cardBox '>
@@ -97,40 +97,41 @@ const Dashboard = () => {
                     </div>
 
 
-                    <div class="row px-4 mt-3">
-                        <h3>Earnings & Payout</h3>
-                        <div class="col-6 ">
-                            <div className='cardBox '>
 
-                                <div className="card gradient-border">
-                                    <div className="card-body d-flex justify-content-between align-items-center">
-                                        <div>
-                                            <h4>₹12000</h4>
-                                            <h5 className="text-s">Activation Wallet</h5>
-                                        </div>
-                                        <img src="asset/logo/28.png" className="l-h" alt="Activation Wallet Logo" />
+                </div>
+                <div class="row px-2 mt-3">
+                    <h3>Earnings & Payout</h3>
+                    <div class="col-6 ">
+                        <div className='cardBox '>
+
+                            <div className="card gradient-border">
+                                <div className="card-body d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <h4>₹12000</h4>
+                                        <h5 className="text-s">Activation Wallet</h5>
                                     </div>
+                                    <img src="asset/logo/28.png" className="l-h" alt="Activation Wallet Logo" />
                                 </div>
                             </div>
-
                         </div>
-                        <div class="col-6 ">
-                            <div className='cardBox '>
-                                <div className="card gradient-border">
-                                    <div className="card-body d-flex justify-content-between align-items-center">
-                                        <div>
-                                            <h4>₹12000</h4>
-                                            <h5 className="text-s">Activation Wallet</h5>
-                                        </div>
-                                        <img src="asset/logo/28.png" className="l-h" alt="Activation Wallet Logo" />
+
+                    </div>
+                    <div class="col-6 ">
+                        <div className='cardBox '>
+                            <div className="card gradient-border">
+                                <div className="card-body d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <h4>₹12000</h4>
+                                        <h5 className="text-s">Activation Wallet</h5>
                                     </div>
+                                    <img src="asset/logo/28.png" className="l-h" alt="Activation Wallet Logo" />
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className='text-center btn-b '>
-                        <button type="button" class="btn btn-dashboard btn-c m-4"> Refer & Earn</button>
-                    </div>
+                </div>
+                <div className='text-center btn-b '>
+                    <button type="button" class="btn btn-dashboard btn-c m-4"> Refer & Earn</button>
                 </div>
 
                 <div className='navbar-b'>
