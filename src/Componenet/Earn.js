6a7958a1af1aaff320e537855cb3ffe672b281cf
hindustantech <145006668+hindustantech@users.noreply.github.com>
2024-10-02 04:11:53 -0700

    final

--- src/Componenet/Earn.js
+++ src/Componenet/Earn.js
@@ -61,7 +61,7 @@ const Earn = () => {
                     </div>
                 </div>
 
-                <div className="p-2">
+                <div className="pt-2">
                     <h4 className="ref-benefits mt-4 px-4">Refer Now</h4>
 
                     <div className="socalmeadia d-flex justify-content-evenly align-items-center">
