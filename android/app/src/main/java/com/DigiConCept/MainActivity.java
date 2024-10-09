adbe5d892594c28f18fc4a7c3753a4ef488ecf0a
hindustantech <145006668+hindustantech@users.noreply.github.com>
2024-10-09 00:57:31 -0700

    Android

---
+++ android/app/src/main/java/com/DigiConCept/MainActivity.java
@@ -0,0 +1,5 @@
+package com.DigiConCept;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.DigiConCept',
+  appName: 'DigiConcept',
+  webDir: 'build'
+};
+
+export default config;
